0ad287b1b70cbdc3241cac4470358291b0621efd
Witold Brzozowski <brzozowskyy@gmail.com>
2020-04-21 14:08:09 +0200

    Add seperate search methods returning parent maps

--- src/_22_ElementaryGraphAlgorithms/BreadthFirstSearch.java
+++ src/_22_ElementaryGraphAlgorithms/BreadthFirstSearch.java
@@ -7,7 +7,7 @@ import static _22_ElementaryGraphAlgorithms.Graph.Edge;
 
 public abstract class BreadthFirstSearch
 {
-    public static <T> List<Edge<T>> breadthFirstSearch(Graph<T> graph, Vertex<T> root) {
+    public static <T> List<Edge<T>> breadthFirstSearchPath(Graph<T> graph, Vertex<T> root) {
         List<Edge<T>> result = new LinkedList<>();
         Queue<Vertex<T>> queue = new LinkedList<>();
         // to check whether a vertex was already visited or not
@@ -30,6 +30,29 @@ public abstract class BreadthFirstSearch
         return result;
     }
 
+    public static <T> Map<Vertex<T>, Vertex<T>> breadthFirstSearchParentMap(Graph<T> graph, Vertex<T> root) {
+        Queue<Vertex<T>> queue = new LinkedList<>();
+        // to check whether a vertex was already visited or not
+        Map<Vertex<T>, Boolean> visited = new HashMap<>();
+        Map<Vertex<T>, Vertex<T>> util = new HashMap<>();
+
+        queue.add(root);
+        visited.put(root, true);
+
+        while (!queue.isEmpty()) {
+            Vertex<T> vertex = queue.poll();
+            for (Vertex<T> v : graph.getAdjacentVerticesOf(vertex)) {
+                if (!visited.containsKey(v)) {
+                    visited.put(v, true);
+                    queue.add(v);
+                    util.putIfAbsent(v, vertex);
+                }
+            }
+        }
+
+        return util;
+    }
+
     private static class Example
     {
         public static void main(String[] args) {
@@ -47,7 +70,8 @@ public abstract class BreadthFirstSearch
 
             System.out.println(graph);
             System.out.println("Breadth first traversal, root: w");
-            System.out.println(BreadthFirstSearch.breadthFirstSearch(graph, new Vertex<>('w')));
+            System.out.println(BreadthFirstSearch.breadthFirstSearchPath(graph, new Vertex<>('w')));
+            System.out.println(BreadthFirstSearch.breadthFirstSearchParentMap(graph, new Vertex<>('w')));
         }
     }
 }
--- src/_22_ElementaryGraphAlgorithms/DepthFirstSearch.java
+++ src/_22_ElementaryGraphAlgorithms/DepthFirstSearch.java
@@ -7,7 +7,7 @@ import static _22_ElementaryGraphAlgorithms.Graph.Edge;
 
 public abstract class DepthFirstSearch
 {
-    public static <T> List<Edge<T>> depthFirstSearch(Graph<T> graph, Vertex<T> root) {
+    public static <T> List<Edge<T>> depthFirstSearchPath(Graph<T> graph, Vertex<T> root) {
         List<Edge<T>> result = new LinkedList<>();
         Stack<Vertex<T>> stack = new Stack<>();
         // to check whether a vertex was already visited or not
@@ -33,6 +33,28 @@ public abstract class DepthFirstSearch
         return result;
     }
 
+    public static <T> Map<Vertex<T>, Vertex<T>> depthFirstSearchParentMap(Graph<T> graph, Vertex<T> root) {
+        Stack<Vertex<T>> stack = new Stack<>();
+        // to check whether a vertex was already visited or not
+        Map<Vertex<T>, Boolean> visited = new HashMap<>();
+        Map<Vertex<T>, Vertex<T>> util = new HashMap<>();
+
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Vertex<T> vertex = stack.pop();
+            if (!visited.containsKey(vertex)) {
+                visited.put(vertex, true);
+                for (Vertex<T> v : graph.getAdjacentVerticesOf(vertex)) {
+                    stack.push(v);
+                    util.putIfAbsent(v, vertex);
+                }
+            }
+        }
+
+        return util;
+    }
+
     private static class Example
     {
         public static void main(String[] args) {
@@ -50,7 +72,8 @@ public abstract class DepthFirstSearch
 
             System.out.println(graph);
             System.out.println("Depth first traversal, root: w");
-            System.out.println(DepthFirstSearch.depthFirstSearch(graph, new Vertex<>('w')));
+            System.out.println(DepthFirstSearch.depthFirstSearchPath(graph, new Vertex<>('w')));
+            System.out.println(DepthFirstSearch.depthFirstSearchParentMap(graph, new Vertex<>('w')));
 
             Graph<Integer> graph2 = new Graph<>(true);
             Integer[] V2 = {0, 1, 2, 3, 4, 5, 6, 7};
@@ -66,7 +89,8 @@ public abstract class DepthFirstSearch
 
             System.out.println("\n" + graph2);
             System.out.println("Depth first traversal, root: 0");
-            System.out.println(DepthFirstSearch.depthFirstSearch(graph2, new Vertex<>(0)));
+            System.out.println(DepthFirstSearch.depthFirstSearchPath(graph2, new Vertex<>(0)));
+            System.out.println(DepthFirstSearch.depthFirstSearchParentMap(graph2, new Vertex<>(0)));
         }
     }
 }
